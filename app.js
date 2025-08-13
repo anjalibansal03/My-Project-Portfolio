0ea18e1b624310b46540d70831cfdd4410b5c9dd
anjalibansal03 <anj10305@gmail.com>
2025-08-13 14:59:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const hamb = document.querySelector('#header .nav-bar .nav-list .hamb');
+const mobile_menu = document.querySelector('#header .nav-bar .nav-list ul');
+const header = document.querySelector('#header');
+
+
+hamb.addEventListener('click', () => {
+    hamb.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+    document.body.classList.toggle('no-scroll');
+});
+
+document.addEventListener('scroll', () => {
+    var scroll_position = window.scrollY;
+    if (scroll_position > 250) {
+        header.style.backgroundColor = '#29323c';
+    } else {
+        header.style.backgroundColor = 'transparent';
+    } 
+});
